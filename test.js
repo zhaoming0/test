44818ca0212449dc03bce123e03d0cf3ae65e990
zhaoming0 <mingx.zhao@intel.com>
2018-10-12 16:46:59 +0800

    update

--- test.js
+++ test.js
@@ -3,8 +3,9 @@ const webdriver = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 const chromedriver = require("chromedriver");
 
+const csv = require("fast-csv");
+
 const until = webdriver.until;
-const By = webdriver.By;
 
 const options = new chrome.Options();
 options.addArguments("no-sandbox");
@@ -13,18 +14,52 @@ const builder = new webdriver.Builder();
 builder.forBrowser("chrome");
 builder.setChromeOptions(options);
 
-console.log(__dirname)
-console.log("123")
-console.log(process.cwd())
-let filepath = path.join("file:\/\/", __dirname, "test", "index-local.html")
-console.log(filepath)
+let testlink = path.join("file:\/\/", __dirname, "test", "index-local.html")
 
 const driver = builder.build();
 (async () => {
     // await driver.get("https://www.google.com");
-    await driver.get(filepath);
+    // await driver.get(testlink);
+    await MODULE_CHROME.open(testlink);
     // await driver.get("file:///home/test/workspace/github/test/test/test/index-local.html");
     // await driver.wait(until.titleIs("Google"));
     // const login = await driver.findElement(By.id("gbqfbb"));
     // await login.click();
 })();
+
+class chrome_module {
+    constructor() {
+        this.builder = require("./node_modules/selenium-webdriver").Builder;
+        this.by = require("./node_modules/selenium-webdriver").By;
+        this.key = require("./node_modules/selenium-webdriver").Key;
+        this.until = require("./node_modules/selenium-webdriver").Until;
+        this.chrome = require("./node_modules/selenium-webdriver/chrome");
+        this.driver;
+        this.options;
+        this.browserPath;
+        this.browserNewest = false;
+    }
+    async open(URL) {
+        await this.driver.get(URL);
+    }
+    async wait(times) {
+        await this.driver.sleep(times);
+    }
+    async close() {
+        await this.driver.close();
+    }
+}
+MODULE_CHROME = new chrome_module;
+class module_csv {
+    constructor() {
+        this.csv = require("./node_modules/fast-csv");
+        this.csvStream;
+    }
+    async write(data) {
+        await this.csvStream.write(data);
+    }
+    async close() {
+        await this.csvStream.end();
+    }
+}
+MODULE_CSV = new module_csv;
